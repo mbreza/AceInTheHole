064635f148be9d939c94d2735f78d81f62f7669b
mbreza <breza.mateusz@gmail.com>
2017-11-16 17:10:07 +0100

    Update MyRMIServer.java

--- src/main/java/ace/server/MyRMIServer.java
+++ src/main/java/ace/server/MyRMIServer.java
@@ -8,7 +8,6 @@ public class MyRMIServer{
 
 	public static void main(String[] args) throws Exception {
 
-    	//System.setProperty("java.rmi.server.hostname", "83.0.171.108 tib2-test");
     	System.setProperty("java.security.policy","file:/home/xyz/AceInTheHole/AceInTheHole/resources/rmi_connection.policy");
     	
         if (System.getSecurityManager() == null) {
