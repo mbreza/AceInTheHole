449246061dc4059b14f1ab30a1dfacae1f15d442
mbreza <breza.mateusz@gmail.com>
2017-11-16 14:48:21 +0100

    asdf

--- src/main/java/ace/server/MyRMIServer.java
+++ src/main/java/ace/server/MyRMIServer.java
@@ -18,7 +18,7 @@ public class MyRMIServer{
         	String name = "tib2-test";
             MyCalc engine = new MyCalcImpl();
             MyCalc stub = (MyCalc) UnicastRemoteObject.exportObject(engine, 0);
-            Registry registry = LocateRegistry.getRegistry(name, serverPort);
+            Registry registry = LocateRegistry.getRegistry(name, 1099);
             System.out.println("Registering Calculator Object");
             registry.rebind(name, stub);
        
